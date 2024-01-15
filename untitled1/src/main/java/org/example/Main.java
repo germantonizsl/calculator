cbf059e39bfcf9662a81350a21b7fcef480e25f8
PavelGermanovich <germanovichpavel95@gmail.com>
2024-01-16 00:03:57 +0300

    hellp

--- untitled1/src/main/java/org/example/Main.java
+++ untitled1/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 import java.util.Scanner;
 public class Main {
     public  static void main(String[] args) {
-
+//hello
         Scanner scanner=new Scanner(System.in);
         System.out.println("ПЕРВОЕ ЧИСЛО");
         int n1=scanner.nextInt();
